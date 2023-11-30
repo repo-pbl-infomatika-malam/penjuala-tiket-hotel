5d551c3357f87f7077d606dbdb0e20c2115a1d03
Afrizal <4friizal@gmail.com>
2023-11-30 23:42:03 +0700

    feat(navbar) : update new navbar

---
+++ view/landing-page/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0">
+  <title>Tiket AjA</title>
+  <link href="../../assets/css/bootstrap.min.css"
+    rel="stylesheet">
+</head>
+
+<body>
+  <nav class=" navbar navbar-expand-lg bg-success">
+    <div class="container-lg">
+      <a class="navbar-brand text-light fs-4 fw-semibold"
+        href="../index.php">Tiket Ajah</a>
+
+      <div id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="../login"
+              class="btn btn-outline-light"
+              type="submit">Login</a>
+          </li>
+          <li class="nav-item ms-2">
+            <a href="../register "
+              class="btn btn-outline-light"
+              type="submit">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  </div>
+</body>
+
+</html>
