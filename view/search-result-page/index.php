eb6b988b960549f60ced5c391733aa45441f6b58
Yusuf Daniswara <yusufdani9@gmail.com>
2023-12-01 16:30:48 +0700

    feat : search-page

--- view/search-result-page/index.php
+++ view/search-result-page/index.php
@@ -5,10 +5,32 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Search Hotel</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <style>
+    .category-checkbox {
+      margin-bottom: 5px; 
+    }
+
+    body {
+      background-color: #333;
+    }
+
+    .container-lg {
+      background-color: #444; 
+      border-radius: 10px; 
+      padding: 20px; 
+      margin-top: 20px; 
+    }
+
+    .list-group-item {
+      background-color: #555; 
+      outline: 2px solid #666;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    }
+  </style>
 </head>
 <body>
 
-<div class="container mt-4">
+<div class="container-lg mt-4">
   <div class="row">
     <div class="col-md-3">
       <div class="form-group">
@@ -33,12 +55,80 @@
 
     <div class="col-md-3 align-self-end">
       <div class="form-group mb-0">
-    <button type="submit" class="btn btn-primary col-md-8">Search</button>
+        <button type="submit" class="btn btn-primary col-md-8">Search</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-lg mt-3">
+  <div class="btn-group ms-5 col-md-1" role="group" aria-label="Basic checkbox toggle button group">
+    <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+    <label class="btn btn-outline-primary" for="btncheck1">Sort</label>
+  </div>
+
+  <div class="btn-group ms-4 " role="group" aria-label="Basic checkbox toggle button group">
+    <div class="btn-group">
+      <button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Top Reviewed
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Solo Travelers</a></li>
+        <li><a class="dropdown-item" href="#">Couples</a></li>
+        <li><a class="dropdown-item" href="#">Groups</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="btn-group ms-4 " role="group" aria-label="Basic checkbox toggle button group">
+    <div class="btn-group">
+      <button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Prices
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Highest</a></li>
+        <li><a class="dropdown-item" href="#">Lowest</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="btn-group ms-4 col-md-2" role="group" aria-label="Basic checkbox toggle button group">
+    <input type="checkbox" class="btn-check" id="btncheck4" autocomplete="off">
+    <label class="btn btn-outline-primary" for="btncheck4">Best Match</label>
+  </div>
+
+
+  <div class="btn-group ms-4 col-md-2" role="group" aria-label="Basic checkbox toggle button group">
+    <input type="checkbox" class="btn-check" id="btncheck5" autocomplete="off">
+    <label class="btn btn-outline-primary" for="btncheck5">Hot Deals!</label>
+    </div>
+
+    <div class="container-lg mt-4">
+  <div class="col-md-2 ms-0">
+    <ul class="list-group list-group-flush p-2 pt-4">
+      <li class="list-group-item" style="background-color: #0077ff;outline: 2px solid #00A8E8; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">
+        <i class="fas fa-list"></i> ⭐⭐⭐⭐⭐
+      </li>
+      <li class="list-group-item" style="background-color: #0059bf; outline: 2px solid #00A8E8; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">
+        <i class="fas fa-angle-right"></i> ⭐⭐⭐⭐
+      </li>
+      <li class="list-group-item" style="background-color: #004191; outline: 2px solid #0066b3; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">
+        <i class="fas fa-angle-right"></i> ⭐⭐⭐
+      </li>
+      <li class="list-group-item" style="background-color: #002752; outline: 2px solid #004578; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">
+        <i class="fas fa-angle-right"></i> ⭐⭐
+      </li>
+      <li class="list-group-item" style="background-color: #001428; outline: 2px solid #00203a; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">
+        <i class="fas fa-angle-right"></i> ⭐⭐
+      </li>
+      <li class="list-group-item" style="background-color: #00071c; outline: 2px solid #00112b; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">
+        <i class="fas fa-angle-right"></i> ⭐
+      </li>
+    </ul>
   </div>
 </div>
 
-  </div>
-</div>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
