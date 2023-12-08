a1cbb3973ec1b8e388ca5c0d75a78ff51a71ed0a
Yustinovianty <76085609+Yustinovianty@users.noreply.github.com>
2023-12-08 19:40:26 +0700

    add modal form input data room

--- view/crud/hapusproduk.php
+++
@@ -1,7 +0,0 @@
-<?php
-// include database connection file
-include 'koneksi.php';
-$id = $_GET['id'];
-$result = mysqli_query($koneksi, "DELETE FROM produk WHERE id='$id'");
-header("Location:index.php");
-?>
