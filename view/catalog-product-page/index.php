ba5c7b7007c477d411379a35a44d3ac6a862c357
Yusuf Daniswara <yusufdani9@gmail.com>
2023-12-09 15:38:39 +0700

    feat : catalog-product-page

--- view/catalog-product-page/index.php
+++ view/catalog-product-page/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hotel Room Catalog</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
@@ -22,7 +22,7 @@
         <!-- Room 1 -->
         <div class="col">
             <div class="card h-100">
-                <img src="https://static01.nyt.com/images/2019/03/24/travel/24trending-shophotels1/24trending-shophotels1-articleLarge.jpg?quality=75&auto=webp&disable=upscale" class="card-img-top" alt="Room 1">
+                <img src="../../assets/images/catalog-product/room1.jpg" class="card-img-top" alt="Room 1">
                 <div class="card-body">
                     <h5 class="card-title">Room 1</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi, sint officia omnis quis at odit esse eaque non quo est tempora saepe dolorum provident placeat labore fuga nisi a.</p>
@@ -43,7 +43,7 @@
         <!-- Room 2 -->
         <div class="col">
             <div class="card h-100">
-                <img src="https://media.cnn.com/api/v1/images/stellar/prod/140127103345-peninsula-shanghai-deluxe-mock-up.jpg?q=w_2226,h_1449,x_0,y_0,c_fill" class="card-img-top" alt="Room 1">
+                <img src="../../assets/images/catalog-product/room2.jpg" class="card-img-top" alt="Room 2">
                 <div class="card-body">
                     <h5 class="card-title">Room 2</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi, sint officia omnis quis at odit esse eaque non quo est tempora saepe dolorum provident placeat labore fuga nisi a.</p>
@@ -64,7 +64,7 @@
         <!-- Room 3 -->
         <div class="col">
             <div class="card h-100">
-                <img src="https://www.acaciahotelsmanila.com/wp-content/uploads/2023/05/Rooms-and-Suites-AHM-1.jpg" class="card-img-top" alt="Room 1">
+                <img src="../../assets/images/catalog-product/room3.jpg" class="card-img-top" alt="Room 3">
                 <div class="card-body">
                     <h5 class="card-title">Room 3</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi, sint officia omnis quis at odit esse eaque non quo est tempora saepe dolorum provident placeat labore fuga nisi a.</p>
@@ -85,7 +85,7 @@
         <!-- Room 4 -->
         <div class="col">
             <div class="card h-100">
-                <img src="https://www.europahotelbelfast.com/wp-content/uploads/2021/10/DSC_7769-Edit-1-1366x768-fp_mm-fpoff_0_0.png" class="card-img-top" alt="Room 1">
+                <img src="../../assets/images/catalog-product/room4.png" class="card-img-top" alt="Room 4">
                 <div class="card-body">
                     <h5 class="card-title">Room 4</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi, sint officia omnis quis at odit esse eaque non quo est tempora saepe dolorum provident placeat labore fuga nisi a.</p>
@@ -106,7 +106,7 @@
         <!-- Room 5 -->
         <div class="col">
             <div class="card h-100">
-                <img src="https://www.seleqtionshotels.com/content/dam/seleqtions/Connaugth/TCPD_PremiumBedroom4_1235.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg" class="card-img-top" alt="Room 1">
+                <img src="../../assets/images/catalog-product/room5.jpeg" class="card-img-top" alt="Room 5">
                 <div class="card-body">
                     <h5 class="card-title">Room 5</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi, sint officia omnis quis at odit esse eaque non quo est tempora saepe dolorum provident placeat labore fuga nisi a.</p>
@@ -127,7 +127,7 @@
         <!-- Room 6 -->
         <div class="col">
             <div class="card h-100">
-                <img src="https://www.bestwesternplusmeridian.com/Content/images/Queen-Room.jpg" class="card-img-top" alt="Room 1">
+                <img src="../../assets/images/catalog-product/room6.jpg" class="card-img-top" alt="Room 6">
                 <div class="card-body">
                     <h5 class="card-title">Room 6</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi, sint officia omnis quis at odit esse eaque non quo est tempora saepe dolorum provident placeat labore fuga nisi a.</p>
@@ -150,6 +150,6 @@
     </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="../../assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
