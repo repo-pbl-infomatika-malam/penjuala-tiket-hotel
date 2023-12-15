fb10c27df4dc195900e9454eeddaee625e937a7e
Yustinovianty <76085609+Yustinovianty@users.noreply.github.com>
2023-12-15 19:08:48 +0700

    feat : change config for connect to database

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $conn = mysqli_connect("localhost:3307", "root", "", "hotel_ticket_management");
 
 if (!$conn) {
-  echo "failed to connected";
-} else {
-  echo "success connected";
+  die("<script>alert('Connection Failed.')</script>");
 };
+
+?>
