4b6ffc779946416cc567f7cc5611835de6f1d447
XenonightX <adjayaandre@gmail.com>
2023-12-22 15:58:41 +0700

    feat : slicing edit profile page

---
+++ view/edit-profile-page/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon"
+    href="../../assets/favicon.ico">
+  <title>Tiket AjA</title>
+  <link rel="stylesheet"
+    type="text/css"
+    href="../../assets/fontawesome/css/all.min.css">
+  <link href="../../assets/css/bootstrap.min.css"
+    rel="stylesheet">
+  <link href="_landing-page.css"
+    rel="stylesheet" />
+  <link href="../../styles/global.css"
+    rel="stylesheet" />
+</head>
+<body>
+      <!-- navbar start -->
+  <nav class="container-navbar">
+    <div class="navbar-content container-lg">
+      <a href="">
+        <img src="../../assets/logo-pbl.png"
+          class="logo"
+          alt="" />
+      </a>
+
+      <div class="d-flex align-items-center gap-2">
+        <div>
+          <a href="#about-us"
+            class="text-button-primary">About Us</a>
+          <a href="../catalog-product-page/"
+            class="text-button-primary">Catalog</a>
+        </div>
+
+        <div class="divider"></div>
+
+        <a href="../login/">
+          <button class="button-primary">Sign In</button>
+        </a>
+        <a class="button-secondary">Sign Up</a>
+      </div>
+    </div>
+  </nav>
+    <br>
+<div class="container">
+    <h1>Edit Profile</h1>
+  	<hr>
+	<div class="row">
+      <!-- left column -->
+      <div class="col-md-3">
+        <div class="text-center">
+        <img src="../../assets/images/avatar.png" class="rounded-3" style="width: 150px;"
+  alt="Avatar" />
+        </div>
+      </div>
+      
+      <!-- edit form column -->
+      <div class="col-md-9 personal-info">
+        <div class="alert alert-info alert-dismissable">
+
+        Ini adalah peringatan. Gunakan ini untuk menampilkan pesan penting.
+        </div>
+        <center><span class="h5">Data Diri</span></center>
+        <br>
+
+        <form class="form-horizontal" role="form">
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Nama Pengguna:</label>
+            <div class="col-lg-8">
+              <input class="form-control" type="text"  placeholder="Masukkan nama pengguna">
+            </div>
+            <br>
+
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Email:</label>
+            <div class="col-lg-8">
+              <input class="form-control" type="text" placeholder="Masukkan alamat email">
+            </div>
+          </div>
+          <br>
+
+          <div class="form-group">
+            <label class="col-lg-3 control-label">No.Handphone:</label>
+            <div class="col-lg-8">
+              <input class="form-control" type="text" placeholder="Masukkan no.handphone">
+            </div>
+          </div>
+
+</div>
+         
+          <div class="form-group">
+            <label class="col-md-3 control-label"></label>
+            <div class="col-md-8">
+            <button class="button-secondary">Perbarui</button>
+              <span></span>
+              <button class="button-primary">Batalkan</button>
+            </div>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
+<hr>
+
+<!-- footer start -->
+<footer class=" border-top">
+    <div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 container-lg">
+      <p class="col-md-4 mb-0 text-body-secondary">&copy; 2023 PBL-IF-D-MALAM, Mhs</p>
+    </div>
+  </footer>
+  <!-- footer end -->
+</body>
+</html>
